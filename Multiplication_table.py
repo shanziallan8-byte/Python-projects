6a8514ecfd3b220942ceacdb29fcf50d81230124
Allan Shanzi <shanziallan8@gmail.com>
2025-09-23 17:24:59 +0200

    Day 11: For loops- multiplication table

---
+++ Multiplication_table.py
@@ -0,0 +1,13 @@
+num = int(input("Enter a number: "))
+
+print(f"\nMultiplication Table for {num}:")
+for i in range(1, 11):
+  print(f"{num} x {i} = {num * i}")
+print("\nMultilplication Tables from q to 5\n")
+for i in range(1, 6):
+  print(f"--- Table of {num} ---")
+print()
+
+
+
+
