d8755ff48f2a7bdd84c028debfe67d661329fe7c
Allan Shanzi <shanziallan8@gmail.com>
2025-09-24 08:10:16 +0200

    Day 7:Python input and formatted output

---
+++ user_profile.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+language = input("Enter your favorite programming language: ")
+hobby = input("enter your hobby: ")
+
+print(f"Hello {name}!")
+print(f"you are: {age}. ")
+print(f"your favorite programming language is {language} and your hobby is {hobby}. ")
+
